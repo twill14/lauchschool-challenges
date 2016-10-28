ec25a8527aeecafca918b03fdaf385f531e4c63a
Thomas Williams <twill14@gmail.com>
2016-10-28 12:07:08 -0500

    Update trinary.rb

--- easy_level_one/trinary.rb
+++ easy_level_one/trinary.rb
@@ -1,25 +1,22 @@
-
-require 'pry'
 class Trinary
-    
     NUMBER = 3
     def initialize(num)
-        @num = num.split('').map(&:to_i)
+        @num = num
     end
     
     def to_decimal
       return 1 if @num == [1]
-      return 0 if invalid_trinary? @num
+      return 0 if invalid_trinary?(@num.to_s)
+        @num = @num.split('').map(&:to_i)
         value = 0
         exponent = @num.size - 1
         counter = 0
         while counter < @num.size
           value += @num[counter]*(NUMBER**exponent)
-          #binding.pry
           counter += 1
           exponent -= 1
         end
-      puts value
+      value
     end
     
     private
@@ -28,7 +25,3 @@ class Trinary
         num.match(/[^0-2]/)
     end
 end
-
-thing = Trinary.new('102012')
-
-thing.to_decimal
